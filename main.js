7e9071f26009192d822151f4f68b75917597d7ec
alien_jedi <lucasrpatten@gmail.com>
2022-04-30 14:27:25 -0600

    Added Page Routing

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+    <React.StrictMode>
+    <HashRouter>
+    <Routes>
+    <Route path="/" element={<App/>} />
+    <Routes>
+    </HashRouter>
+    </React.StrictMode>
+
+    document.getElementById('app'));
